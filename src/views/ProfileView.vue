e08a9b428bea55687d6bd7dfc7c71a8b11ccb9fe
marvin.sendikaddiwa@interswitchgroup.com <marvin.sendikaddiwa@interswitchgroup.com>
2022-06-12 23:32:21 +0300

    Add view profile template

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,9 +1,40 @@
 <template>
-  <div class="Profile">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <!-- <Profile msg="Welcome to Your Vue.js App"/> -->
+    <br>
+    <br>
+    <br>
+    <br>
+  <div  class="profile">
+  <div class="container">
+    <h1>Profile</h1>
+    <p>Below is your profile information</p>
+    <hr>
+    <b>First Name</b> : {{ firstName }}
+    <br>
+    <b>Last Name</b> : {{ lastName }}
+    <br>
+    <b>Phone Number</b> : {{ phoneNumber }}
+    <br>
+    <b>Created At</b> : {{ createdAt }}
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <button class="registerbtn" @click="logout">Logout</button>
   </div>
+</div>
 </template>
+
+<style>
+  
+.profile {
+  max-width: 800px;
+  margin: auto;
+  padding: auto;
+}
+</style>
 
 <script>
 // @ is an alias to /src
@@ -13,8 +44,20 @@ export default {
   name: 'ProfileView',
   components: {
     Profile
+  },
+  data() {
+    return {
+      firstName: 'Marvin',
+      lastName: 'Sendikaddiwa',
+      phoneNumber: '256702544870',
+      createdAt: '2022-06-12',
+    }
+  },
+  methods: {
+    logout () {
+      // console.log({hello: this.phoneNumber})
+      this.$router.push({path: '/login'});  
+    }
   }
 }
-
-// this.$router.push({path: '/login'});
 </script>
